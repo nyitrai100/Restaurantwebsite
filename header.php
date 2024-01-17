5287fac6d79d70bad911d24e5ddcc0c0d8dca954
nyitrai100 <nyitrai100@gmail.com>
2024-01-17 18:51:09 +0000

    tailwind connected

--- header.php
+++ header.php
@@ -9,25 +9,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
     <title>Restaurant</title>
 </head>
 <body>  
-      <!-- <nav class="nav-container mb-2">
-            <ul class="nav justify-content-end">
-                <li class="nav-item">
-                <a class="nav-link" href="./index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                <?php if(isset($_SESSION["user"]))
-                {echo "<a class='nav-link' href='./browser.php'>Browser</a>"; } ?>
-                
-                </li>
-                <li class="nav-item">
-                <?php if(isset($_SESSION["user"]))
-                {echo "<a class='nav-link' href='./Authentication/logout.php'>Log out</a>"; }
-                       else{echo " <a class='nav-link' href='#' onclick='openLogin()'>Login</a>"; }       ?>           
-                </li>
-            </ul>
-        </nav> -->
+
+
+        
--- index.php
+++ index.php
@@ -3,7 +3,9 @@
   include("header.php");
 ?>
 
-
+<h1 class="text-1xl font-bold underline">
+    Hello world!
+  </h1>
 
 <?php 
 // include("footer.php");
