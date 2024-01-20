d5bd82e69054f905e520628be01e6b1e7306105e
nyitrai100 <nyitrai100@gmail.com>
2024-01-20 16:10:59 +0000

    reservation searchbar

--- reservation.php
+++ reservation.php
@@ -5,6 +5,50 @@
 
 
 
+<div class="pt-10 md:p-20 bg-black flex justify-center">
+
+<div class="m-2 max-w-screen ">
+  <div class="rounded-xl border border-gray-200 bg-white p-6 shadow-lg">
+    <h2 class="text-stone-700 text-xl font-bold">Apply filters</h2>
+    <p class="mt-1 text-sm">Use filters to further refine search</p>
+    <div class="mt-8 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div class="flex flex-col">
+        <label for="name" class="text-stone-600 text-sm font-medium">Name</label>
+        <input type="text" id="name" placeholder="John Doe" class="mt-2 block w-full rounded-md border border-gray-200 px-2 py-2 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50" />
+      </div>
+
+      <div class="flex flex-col">
+        <label for="email" class="text-stone-600 text-sm font-medium">Email</label>
+        <input type="email" id="email" placeholder="example@gmail.com" class="mt-2 block w-full rounded-md border border-gray-200 px-1 py-2 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50" />
+      </div>
+
+      <div class="flex flex-col">
+        <label for="date" class="text-stone-600 text-sm font-medium">Booked Date</label>
+        <input type="date" id="date" class="mt-2 block w-full rounded-md border border-gray-200 px-2 py-2 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50" />
+      </div>
+
+      <!-- <div class="flex flex-col">
+        <label for="status" class="text-stone-600 text-sm font-medium">Status</label>
+
+        <select id="status" class="mt-2 block w-full rounded-md border border-gray-200 px-2 py-2 shadow-sm outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
+          <option>Dispached Out</option>
+          <option>In Warehouse</option>
+          <option>Being Brought In</option>
+        </select>
+      </div> -->
+    </div>
+
+    <div class="mt-6 grid w-full grid-cols-2 justify-end space-x-4 md:flex">
+      <button class="active:scale-95 rounded-lg bg-gray-200 px-8 py-2 font-medium text-gray-600 outline-none focus:ring hover:opacity-90">Reset</button>
+      <button class="active:scale-95 rounded-lg bg-blue-600 px-8 py-2 font-medium text-white outline-none focus:ring hover:opacity-90">Search</button>
+    </div>
+  </div>
+</div>
+
+</div>
+
+
+
 <div class="p-10 md:p-20 bg-black">
 <div class="overflow-x-auto rounded">
   <table class="min-w-full bg-white font-[sans-serif]">
