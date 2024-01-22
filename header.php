c6b2dc64b57335a48f5669fc2c0f9a6b9260bdd3
nyitrai100 <nyitrai100@gmail.com>
2024-01-22 13:26:11 +0000

    flowbite works

--- header.php
+++ header.php
@@ -11,6 +11,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
     <script src="javascript.js"></script>
     <title>Restaurant</title>
 </head>
