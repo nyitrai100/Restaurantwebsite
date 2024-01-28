d941d5900b189b7e4b6a46e7aca0b3bfc1d29a60
nyitrai100 <nyitrai100@gmail.com>
2024-01-28 19:31:25 +0000

    database connection

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+try {
+    $conn = new PDO('mysql:host=localhost;port=8100;dbname=restaurant-TeamProject', 'root', '');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } 
+  catch (PDOException $exception) {
+  echo "Oh no, there was a problem" . $exception->getMessage();
+  }
+  
