d9c54db67612a639a3c7ff21a57c653aa6209236
nyitrai100 <nyitrai100@gmail.com>
2024-02-03 09:57:04 +0000

    new db connected, login works with 4 emails

--- Authentication/login_process.php
+++ Authentication/login_process.php
@@ -2,7 +2,7 @@
 session_start();
 
 try {
-    $conn = new PDO('mysql:host=localhost;port=8100;dbname=restaurant-TeamProject', 'root', '');
+    $conn = new PDO('mysql:host=localhost;port=8100;dbname=TheGrillSmith', 'root', '');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $exception) {
     echo "Oh no, there was a problem" . $exception->getMessage();
