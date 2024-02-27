523abe62c28ade433ab34d260000cb85c0ff98bb
nyitraibence <nyitrai100@gmail.com>
2024-02-27 11:50:12 +0000

    links updated.

--- Authentication/login_process.php
+++ Authentication/login_process.php
@@ -3,7 +3,7 @@ include("./database.php");
 session_start();
 
 try {
-    $conn = new PDO('mysql:host=localhost;port=3306;dbname=thegrillsmith', 'root', '');
+    $conn = new PDO('mysql:host=localhost;port=8100;dbname=TheGrillSmith', 'root', '');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $exception) {
     echo "Oh no, there was a problem" . $exception->getMessage();
--- Manage_Booking/get_available_timeslots.php
+++ Manage_Booking/get_available_timeslots.php
@@ -1,7 +1,7 @@
 
 <?php
 try {
-    $conn = new PDO('mysql:host=localhost;port=3306;dbname=thegrillsmith', 'root', '');
+    $conn = new PDO('mysql:host=localhost;port=8100;dbname=TheGrillSmith', 'root', '');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $exception) {
     echo "Oh no, there was a problem" . $exception->getMessage();
